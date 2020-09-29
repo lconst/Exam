3bef026c8e520d22a7056449c170b042d294ab25
e.zaharova <zakh14@bk.ru>
2020-09-29 15:43:09 +0300

    add viewModel

--- app/src/main/java/com/example/exam/questions/QuestionsFragment.kt
+++ app/src/main/java/com/example/exam/questions/QuestionsFragment.kt
@@ -1,6 +1,18 @@
 package com.example.exam.questions
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class QuestionsFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
---
+++ app/src/main/java/com/example/exam/questions/QuestionsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.exam.questions
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class QuestionsViewModel(application: Application): AndroidViewModel(application) {
+}
